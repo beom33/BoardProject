7c9d211e7beec960f814336ce99602dfbd0a8683
beom33 <byby5546@naver.com>
2024-06-25 16:05:30 +0900

    3차프로젝트

---
+++ src/main/java/org/choongang/member/validators/JoinValidator.java
@@ -0,0 +1,31 @@
+package org.choongang.member.validators;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.choongang.global.config.annotations.Component;
+import org.choongang.global.validators.EmailValidator;
+import org.choongang.global.validators.RequiredValidator;
+import org.choongang.global.validators.Validator;
+import org.choongang.member.controllers.RequestJoin;
+import org.choongang.member.mappers.MemberMapper;
+
+@Component
+@RequiredArgsConstructor
+public class JoinValidator implements Validator<RequestJoin>, RequiredValidator,
+        EmailValidator {
+
+    private final MemberMapper mapper;
+
+    @Override
+    public void check(RequestJoin form) {
+        String email = form.getEmail();
+        String password = form.getPassword();
+        String confirmPassword = form.getConfirmPassword();
+        String userName = form.getUserName();
+        boolean termsAgree = form.isTermsAgree();
+            int status = HttpServletResponse.SC_UNAUTHORIZED;
+
+
+    }
+}
