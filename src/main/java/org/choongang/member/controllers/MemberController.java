efdeb034dc6e9eb1c6571f50c083b7046dd2137c
beom33 <byby5546@naver.com>
2024-06-26 10:24:40 +0900

    3차프로젝트 수업

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,11 +1,13 @@
 package org.choongang.member.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Controller;
 import org.choongang.global.config.annotations.GetMapping;
 import org.choongang.global.config.annotations.PostMapping;
 import org.choongang.global.config.annotations.RequestMapping;
 import org.choongang.member.services.JoinService;
+
 
 @Controller
 @RequestMapping("/member")
@@ -14,7 +16,6 @@ public class MemberController {
 
     private final JoinService joinService;
 
-
     // 회원 가입 양식
     @GetMapping("/join")
     public String join() {
@@ -22,26 +23,30 @@ public class MemberController {
         return "member/join";
     }
 
-    //회원 가입 처리
+    // 회원 가입 처리
     @PostMapping("/join")
-    public String joinPs(RequestJoin form) {
+    public String joinPs(RequestJoin form, HttpServletRequest request) {
 
-        joinService.prcess(form);
+        joinService.process(form);
 
-        return "memeber/join";
+        String url = request.getContextPath() + "/member/login";
+        String script = String.format("parent.location.replace('%s');", url);
+
+        request.setAttribute("script", script);
+
+        return "commons/execute_script";
     }
 
-    //로그인 양식
+    // 로그인 양식
     @GetMapping("/login")
     public String login() {
 
-        return  "member/login";
+        return "member/login";
     }
 
-    //로그인 처리
+    // 로그인 처리
     @PostMapping("/login")
-    public  String loginPs(RequestLogin form) {
-
+    public String loginPs(RequestLogin form) {
 
         return null;
     }
