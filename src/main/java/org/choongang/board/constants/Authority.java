8d1ed3cfbd5546405c50a2490fb4c4fa37644d71
beom33 <byby5546@naver.com>
2024-06-27 11:06:38 +0900

    3차프로젝트 수업(게시판 기능 추가)

---
+++ src/main/java/org/choongang/board/constants/Authority.java
@@ -0,0 +1,8 @@
+package org.choongang.board.constants;
+
+public enum Authority {
+    ALL,
+    USER,
+    ADMIN,
+
+}
--- src/main/java/org/choongang/member/MemberUtil.java
+++ src/main/java/org/choongang/member/MemberUtil.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.choongang.global.config.annotations.Component;
+import org.choongang.global.config.containers.BeanContainer;
 import org.choongang.member.constants.UserType;
 import org.choongang.member.entities.Member;
 
@@ -11,7 +12,7 @@ import org.choongang.member.entities.Member;
 @RequiredArgsConstructor
 public class MemberUtil {
     private final HttpSession session;
-
+    //로그인 여부
     public boolean isLogin() {
         return getMember() != null;
 
@@ -35,6 +36,7 @@ public class MemberUtil {
      */
 
     public Member getMember() {
+        HttpSession session = BeanContainer.getInstance().getBean(HttpSession.class);
     Member member = (Member)session.getAttribute("member");
 
     return member;
